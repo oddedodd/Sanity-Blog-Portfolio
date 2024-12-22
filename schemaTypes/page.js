11e38cf06a52a09c19a0c8b4a0933d1deebe5b31
Odd Egil Hegge Selnes <post@odde.org>
2024-12-22 20:22:47 +0100

    added Content type Page for later setting up the content of the static pages

---
+++ schemaTypes/page.js
@@ -0,0 +1,36 @@
+export default {
+  name: 'page',
+  title: 'Pages',
+  type: 'document',
+  fields: [
+    {
+      name: 'title',
+      title: 'Title',
+      type: 'string',
+      validation: Rule => Rule.required()
+    },
+    {
+      name: 'slug',
+      title: 'Slug',
+      type: 'slug',
+      options: {
+        source: 'title',
+        maxLength: 96
+      },
+      validation: Rule => Rule.required()
+    },
+    {
+      name: 'featuredImage',
+      title: 'Featured Image',
+      type: 'image',
+      options: {
+        hotspot: true // Enables UI for selecting what areas of an image should be cropped
+      }
+    },
+    {
+      name: 'content',
+      title: 'Content',
+      type: 'blockContent' // This uses your existing blockContent type
+    }
+  ]
+}
